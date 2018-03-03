fe722f722ba285c9895213615fe891ff347abf11
TimothyClark <timothyclark.white4@gmail.com>
2018-03-03 13:48:57 +0200

    Push
    First Push

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+
+function myFunction () {
+    document.getElementById("demo_1").innerHTML = "Hello Dolly";
+}
+function myFunctionTwo (){
+    document.getElementById("demo_2").style.fontSize = "25px";
+}
+function myFunctionThree(){
+    document.getElementById("demo_3").style.display = 'block';
+}
+ 
