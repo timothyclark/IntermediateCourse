cdadbb4f2ccfbf7c5bd8494f5888b400d9776cea
timothyclark <36359356+timothyclark@users.noreply.github.com>
2018-03-10 06:38:00 +0200

    Add files via upload

---
+++ script cars.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+function myFunction () {
+    document.getElementById("demo_1").innerHTML = "Start your Engines!!!";
+}
+function myFunctionTwo (){
+    document.getElementById("demo_2").style.fontSize = "25px";
+}
+
+function myFunctionThree (){
+	var x =
+	document.getElementById("myDIV");
+	if(x.style.display==="none"){
+		x.style.display="block";
+	}else{
+		x.style.display="none";
+	}
+}
